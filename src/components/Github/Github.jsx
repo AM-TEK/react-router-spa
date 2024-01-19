4fa2ac8d8a7a25bbb7717f1bb479a2b522e4f87d
Alejandro Maldonado <am@AM-WEBDEV.local>
2024-01-19 17:00:57 -0600

    add links, fetch api data

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import githubLogo from '../../assets/tchaikovsky.jpg'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
+
+  const data = useLoaderData()
+  // const [data, setData] = useState([])
+  // useEffect(() => {
+  //   fetch('https://api.github.com/users/hiteshchoudhary')
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     console.log(data)
+  //     setData(data)
+  //   })
+  // }, [])
+
   return (
-    <div>Github</div>
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+      Github followers: {data.followers}
+      <img src={githubLogo} width={300} />
+    </div>
   )
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+  const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+  return response.json()
+}
